b8b125878674586c505d7501e74f263bb07df864
Logan Wood <lwood3499@gmail.com>
2019-04-02 18:26:07 -0500

    completed gatsby docs 1 - 3, will erase and do section 4

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,11 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
-      <div style={{ color: `teal` }}>
-            <Link to="/">Home</Link>
-            <Header headerText="Contact" />
-            <p>Send a message!</p>
-      </div>
+      <Layout>
+            <h1>I'd love to talk! Email me at the address below</h1>
+            <p>
+                  <a href="mailto:lwood3499@gmail.com">lwood3499@gmail.com</a>
+            </p>
+      </Layout>
 )
